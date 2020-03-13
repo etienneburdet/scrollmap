9cb65db0e1b68795547b2557a7b37ec216a8c9ca
Etienne Burdet <etienne.burdet@gmail.com>
2020-03-13 17:54:30 +0100

    build in subfolder

--- src/app.js
+++
@@ -1,8 +0,0 @@
-import './plugins/ods-widgets/stylesheets/ods-widgets.less'
-import 'mapbox-gl/dist/mapbox-gl.css'
-import './assets/stylesheets/app.scss'
-
-import story from './components/story/story.js'
-import './components/story/story.scss'
-
-story()
